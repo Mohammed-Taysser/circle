3842e45d7edad044a5194fb580e161e1422a6051
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-20 19:18:53 +0200

    finish groups page

--- src/pages/auth/Groups.tsx
+++ src/pages/auth/Groups.tsx
@@ -1,25 +1,77 @@
-import { Center, SegmentedControl } from '@mantine/core';
+import {
+  Button,
+  Center,
+  Input,
+  Pagination,
+  SegmentedControl,
+  Select,
+} from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useDocumentTitle } from '@mantine/hooks';
 import { useState } from 'react';
 import { AiOutlineUnorderedList } from 'react-icons/ai';
-import { BsColumnsGap, BsMegaphoneFill } from 'react-icons/bs';
+import { BiSearchAlt } from 'react-icons/bi';
+import { BsColumnsGap, BsMegaphoneFill, BsSortUpAlt } from 'react-icons/bs';
+import Banner from '../../common/Banner';
 import Group from '../../common/Group';
 import { GROUPS } from '../../constants/dummy';
-import Banner from '../../common/Banner';
-import { useDocumentTitle } from '@mantine/hooks';
 
 function Groups() {
   useDocumentTitle('Mantine | Groups');
   const [view, setView] = useState('list');
+  const [activePage, setPage] = useState(1);
+
+  const searchForm = useForm({
+    initialValues: {
+      query: '',
+      sort: 'alphabetical',
+    },
+  });
+
+  const onFSearchFormSubmit = (values: { query: string }) => {
+    console.log(values);
+  };
 
   return (
     <>
       <Banner
         title='All Groups'
-        subtitle='All Groups 24'
+        subtitle={`All Groups: ${GROUPS.length}`}
         icon={BsMegaphoneFill}
       />
-      <div className='nice-shadow flex justify-end p-4'>
+      <div className='nice-shadow flex justify-between p-5'>
+        <form
+          className='flex items-center flex-1'
+          onSubmit={searchForm.onSubmit(onFSearchFormSubmit)}
+        >
+          <Input
+            icon={<BiSearchAlt />}
+            placeholder='Search Groups'
+            {...searchForm.getInputProps('query')}
+          />
+          <Button variant='light' color='teal' className='mx-3' type='submit'>
+            Search
+          </Button>
+          <Select
+            className='ml-auto inline-block'
+            icon={<BsSortUpAlt />}
+            placeholder='Order By'
+            clearable
+            {...searchForm.getInputProps('sort')}
+            onChange={(value) => {
+              searchForm.setValues({
+                sort: value || 'alphabetical',
+              });
+            }}
+            data={[
+              { value: 'alphabetical', label: 'Alphabetical' },
+              { value: 'new', label: 'Newly Created' },
+              { value: 'member', label: 'Most Members' },
+            ]}
+          />
+        </form>
         <SegmentedControl
+          className='ml-5'
           color='teal'
           value={view}
           onChange={setView}
@@ -51,6 +103,9 @@ function Groups() {
         {GROUPS.map((group) => (
           <Group colView={view === 'col'} group={group} key={group.id} />
         ))}
+        <div className='nice-shadow p-5 flex justify-center my-10'>
+          <Pagination value={activePage} onChange={setPage} total={10} />
+        </div>
       </div>
     </>
   );
