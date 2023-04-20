5e3c20f540af0cad5b9bd5de1b32a5c652b55151
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-20 17:17:37 +0200

    use `useDocumentTitle` hook to change page title

--- src/pages/auth/Profile.tsx
+++ src/pages/auth/Profile.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import { useDocumentTitle } from '@mantine/hooks';
 import Banner from '../../components/profile/Banner.profile';
 import Taps from '../../components/profile/Taps.profile';
 
 import '../../assets/scss/pages/profile.scss';
 
 function Profile() {
+  useDocumentTitle('Mantine | Profile');
   return (
     <div className='profile-page'>
       <Banner />
