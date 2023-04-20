acef37f4212da2803a47f6a19226a3d7dd4a139e
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-20 11:53:12 +0200

    create friend component

---
+++ src/common/Friend.tsx
@@ -0,0 +1,69 @@
+import { Avatar } from '@mantine/core';
+import { ReactElement } from 'react';
+import { Link } from 'react-router-dom';
+import { timeToX } from '../helpers';
+import AvatarA from './Avatar';
+
+/**
+ * Friend component
+ * @usage
+
+- use `className` to add classes to Friend wrapper
+- use `user` to pass friend info
+
+ * @returns {React.ReactElement}
+ */
+function Friend(props: { user: Friend; className: string }): ReactElement {
+  if (!props.user) {
+    return <></>;
+  }
+
+  return (
+    <div className={`friend ${props.className}`}>
+      <img
+        src={props.user.cover}
+        alt={`Cover of ${props.user.name}`}
+        className='cover'
+      />
+      <div className='flex items-center relative'>
+        <Link to={`/profile/${props.user.id}`}>
+          <AvatarA
+            src={props.user.avatar}
+            alt={`Avatar of ${props.user.name}`}
+          />
+        </Link>
+        <div className='mx-12'>
+          <Link
+            to={`/profile/${props.user.id}`}
+            className='text-black text-xl no-underline transition hover:text-aurora'
+          >
+            {props.user.name}
+          </Link>
+          <div className='text-gray-400 mb-1 text-sm'>
+            @{props.user.username}
+          </div>
+          <div className='text-gray-500 text-sm mb-4'>
+            Since: {timeToX(props.user.joinAt)}
+          </div>
+          <Avatar.Group spacing='sm'>
+            {props.user.badges.map((badge) => (
+              <Avatar
+                key={badge.id}
+                radius='xl'
+                src={badge.picture}
+                alt={badge.label}
+              />
+            ))}
+          </Avatar.Group>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Friend.defaultProps = {
+  user: null,
+  className: '',
+};
+
+export default Friend;
