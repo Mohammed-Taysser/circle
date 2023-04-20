6793520ae2a3a26f4261338eae3cfa157f49857e
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-20 13:37:01 +0200

    create dummy home page

---
+++ src/pages/auth/Homepage.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mantine/core'
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+              <Button  >Primary button</Button>
+      <Button color="teal.5">Blue button</Button>
+    </div>
+  )
+}
+
+export default Homepage
