b76010aa11ba03bd52a8d4af7eba42fd07058402
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-22 08:58:33 +0200

    finish homepage

--- src/pages/auth/Homepage.tsx
+++ src/pages/auth/Homepage.tsx
@@ -1,15 +1,37 @@
-import { Button } from '@mantine/core';
 import { useDocumentTitle } from '@mantine/hooks';
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Skeleton from '../../common/Skeleton';
+import Timeline from '../../components/taps/Timeline';
+import { POSTS } from '../../constants/dummy';
+import Async from '../../containers/Async';
 
 function Homepage() {
   useDocumentTitle('Mantine | Homepage');
 
+  const [state, setState] = useState({
+    loading: true,
+    fulfilled: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    const TimerId = setTimeout(() => {
+      setState({
+        loading: false,
+        fulfilled: true,
+        error: null,
+      });
+    }, 2000);
+
+    return () => {
+      clearTimeout(TimerId);
+    };
+  }, []);
+
   return (
-    <div>
-      <Button>Primary button</Button>
-      <Button color='teal.5'>Blue button</Button>
-    </div>
+    <Async {...state} skeleton={<Skeleton.post repeat={6} />}>
+      <Timeline posts={POSTS.timeline} />
+    </Async>
   );
 }
 
