b4f652a2124400fa256f29d6732a218242295a35
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-23 20:40:09 +0200

    create  ColorHighlighter editor plugin

---
+++ src/components/tiptap/ColorHighlighter.ts
@@ -0,0 +1,58 @@
+import { Extension } from '@tiptap/core';
+import { Node } from '@tiptap/pm/model';
+import { Plugin } from '@tiptap/pm/state';
+import { Decoration, DecorationSet } from '@tiptap/pm/view';
+
+const ColorHighlighter = Extension.create({
+  name: 'colorHighlighter',
+
+  addProseMirrorPlugins() {
+    return [
+      new Plugin({
+        state: {
+          init(_, { doc }) {
+            return findColors(doc);
+          },
+          apply(transaction, oldState) {
+            return transaction.docChanged
+              ? findColors(transaction.doc)
+              : oldState;
+          },
+        },
+        props: {
+          decorations(state) {
+            return this.getState(state);
+          },
+        },
+      }),
+    ];
+  },
+});
+
+function findColors(doc: Node): DecorationSet {
+  const hexColor = /(#[0-9a-f]{3,6})\b/gi;
+  const decorations: Decoration[] = [];
+
+  doc.descendants((node, position) => {
+    if (!node.text) {
+      return;
+    }
+
+    Array.from(node.text.matchAll(hexColor)).forEach((match) => {
+      const color = match[0];
+      const index = match.index || 0;
+      const from = position + index;
+      const to = from + color.length;
+      const decoration = Decoration.inline(from, to, {
+        class: 'color',
+        style: `--color: ${color}`,
+      });
+
+      decorations.push(decoration);
+    });
+  });
+
+  return DecorationSet.create(doc, decorations);
+}
+
+export default ColorHighlighter;
