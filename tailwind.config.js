42973685992ec7697e9cee9139d9c7f5ce559dcd
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-04-24 14:14:08 +0200

    add `nice-shadow` as nice variable to tailwindcss shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       colors: {
         aurora: '#20c997',
       },
+      boxShadow: {
+        nice: '0 3px 15px rgba(22, 41, 124, 0.1)',
+      },
     },
   },
   plugins: [],
