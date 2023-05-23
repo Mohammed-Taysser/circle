994dcf267139aa06f388b70db4f7d0bff681583c
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-23 23:10:38 +0300

    create groups page

--- src/pages/public/Groups.tsx
+++ src/pages/public/Groups.tsx
@@ -1,111 +1,64 @@
-import {
-  Button,
-  Center,
-  Input,
-  Pagination,
-  SegmentedControl,
-  Select,
-} from '@mantine/core';
-import { useForm } from '@mantine/form';
-import { useDocumentTitle } from '@mantine/hooks';
-import { useState } from 'react';
-import { AiOutlineUnorderedList } from 'react-icons/ai';
-import { BiSearchAlt } from 'react-icons/bi';
-import { BsColumnsGap, BsMegaphoneFill, BsSortUpAlt } from 'react-icons/bs';
+import { Anchor, Image } from '@mantine/core';
+import { BsMegaphoneFill } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import discoverImage from '../../assets/images/background/discover-groups.svg';
+import yourImage from '../../assets/images/background/your-groups.svg';
+import friendsImage from '../../assets/images/background/friends-groups.svg';
+import recommendedImage from '../../assets/images/background/recommended-groups.svg';
 import Banner from '../../common/Banner';
-import Group from '../../common/Group';
-import { GROUPS } from '../../constants/dummy';
+import { uuidv4 } from '../../helpers';
+
+const GROUPS_TYPES = [
+  {
+    img: discoverImage,
+    title: 'Discover',
+    url: 'discover',
+  },
+  {
+    img: friendsImage,
+    title: 'Friends',
+    url: 'friends',
+  },
+  {
+    img: yourImage,
+    title: 'Your',
+    url: 'your',
+  },
+  {
+    img: recommendedImage,
+    title: 'Recommended',
+    url: 'recommended',
+  },
+];
 
 function Groups() {
-  useDocumentTitle('Mantine | Groups');
-  const [view, setView] = useState('list');
-  const [activePage, setPage] = useState(1);
-
-  const searchForm = useForm({
-    initialValues: {
-      query: '',
-      sort: 'alphabetical',
-    },
-  });
-
-  const onFSearchFormSubmit = (values: { query: string }) => {
-    console.log(values);
-  };
-
   return (
     <>
       <Banner
-        title='All Groups'
-        subtitle={`All Groups: ${GROUPS.length}`}
+        title='Groups'
+        subtitle={`Search open groups`}
         icon={BsMegaphoneFill}
       />
-      <div className='nice-shadow flex justify-between p-5 bg-white'>
-        <form
-          className='flex items-center flex-1'
-          onSubmit={searchForm.onSubmit(onFSearchFormSubmit)}
-        >
-          <Input
-            icon={<BiSearchAlt />}
-            placeholder='Search Groups'
-            {...searchForm.getInputProps('query')}
-          />
-          <Button variant='light' color='teal' className='mx-3' type='submit'>
-            Search
-          </Button>
-          <Select
-            className='ml-auto inline-block'
-            icon={<BsSortUpAlt />}
-            placeholder='Order By'
-            clearable
-            {...searchForm.getInputProps('sort')}
-            onChange={(value) => {
-              searchForm.setValues({
-                sort: value || 'alphabetical',
-              });
-            }}
-            data={[
-              { value: 'alphabetical', label: 'Alphabetical' },
-              { value: 'new', label: 'Newly Created' },
-              { value: 'member', label: 'Most Members' },
-            ]}
-          />
-        </form>
-        <SegmentedControl
-          className='ml-5'
-          color='teal'
-          value={view}
-          onChange={setView}
-          data={[
-            {
-              value: 'col',
-              label: (
-                <Center>
-                  <BsColumnsGap />
-                </Center>
-              ),
-            },
-            {
-              value: 'list',
-              label: (
-                <Center>
-                  <AiOutlineUnorderedList />
-                </Center>
-              ),
-            },
-          ]}
-        />
-      </div>
-      <div
-        className={`my-10 ${
-          view === 'col' ? 'md:grid md:grid-cols-2 lg:grid-cols-3 gap-4' : ''
-        } `}
-      >
-        {GROUPS.map((group) => (
-          <Group colView={view === 'col'} group={group} key={group.id} />
+      <div className='md:grid grid-cols-2 gap-4'>
+        {GROUPS_TYPES.map((groupType) => (
+          <div className='col' key={uuidv4()}>
+            <div className='nice-shadow  p-5 bg-white'>
+              <Image
+                withPlaceholder
+                height={350}
+                src={groupType.img}
+                alt={`${groupType.title}-groups`}
+              />
+              <Anchor
+                component={Link}
+                to={`/groups/${groupType.url}`}
+                className='mt-3 inline-block text-2xl no-underline'
+              >
+                {groupType.title} Groups
+              </Anchor>
+            </div>
+          </div>
         ))}
-        <div className='nice-shadow p-5 flex justify-center my-10 bg-white'>
-          <Pagination value={activePage} onChange={setPage} total={10} />
-        </div>
       </div>
     </>
   );
