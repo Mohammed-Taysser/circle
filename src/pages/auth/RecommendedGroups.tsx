f4161b93e42bb269c96a424e2310f6edeccdd135
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-23 23:11:31 +0300

    create recomments-groups page

---
+++ src/pages/auth/RecommendedGroups.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { BsHandIndexThumb } from 'react-icons/bs';
+import Skeleton from '../../common/Skeleton';
+import { GROUPS } from '../../constants/dummy';
+import Async from '../../containers/Async';
+import GroupsContainer from '../../containers/Groups';
+
+function DiscoverGroups() {
+  const [state, setState] = useState({
+    loading: true,
+    fulfilled: false,
+    error: null,
+  });
+
+  const onSearchFormSubmit = (values: { query: string }) => {
+    console.log(values);
+  };
+
+  useEffect(() => {
+    const TimerId = setTimeout(() => {
+      setState({
+        loading: false,
+        fulfilled: true,
+        error: null,
+      });
+    }, 2000);
+
+    return () => {
+      clearTimeout(TimerId);
+    };
+  }, []);
+
+  return (
+    <Async {...state} skeleton={<Skeleton.post repeat={6} />}>
+      {GROUPS && (
+        <GroupsContainer
+          onSearchFormSubmit={onSearchFormSubmit}
+          title='Recommended Groups'
+          groups={GROUPS}
+          icon={BsHandIndexThumb}
+        />
+      )}
+    </Async>
+  );
+}
+
+export default DiscoverGroups;
