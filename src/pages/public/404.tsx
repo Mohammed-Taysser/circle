a6d4fcc59bb39e050e2680efbdd23139f3c2c34d
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-05-23 23:09:57 +0300

    add login link to 404 page

--- src/pages/public/404.tsx
+++ src/pages/public/404.tsx
@@ -1,17 +1,22 @@
 import { VscArrowSmallLeft } from 'react-icons/vsc';
 import { Link } from 'react-router-dom';
 import PageNotFoundImage from '../../assets/images/background/404.svg';
-import { Button } from '@mantine/core';
+import { Anchor, Button } from '@mantine/core';
 
 function PageNotFound() {
   return (
-    <div className='h-screen text-center'>
+    <div className='text-center'>
       <div className='flex h-full content-center justify-center'>
         <div className='self-center'>
           <img src={PageNotFoundImage} alt='404 page' className='max-w-full' />
           <h2 className='text-lg my-5'>Oops… You just found an error page</h2>
           <h3 className='small text-muted'>
-            We are sorry but the page you are looking for was not found
+            We are sorry but the page you are looking for was not found, or you
+            need to
+            <Anchor component={Link} to='/login' className='mx-1'>
+              Login
+            </Anchor>
+            to see it
           </h3>
           <Button component={Link} to='/' className=' mt-5'>
             <VscArrowSmallLeft className='text-2xl m-0' />
