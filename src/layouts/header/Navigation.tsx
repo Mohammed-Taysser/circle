ba5e15e8d4c79b2c09bcc1968101649d034cd41c
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-02 16:23:53 +0300

    create header navigation

---
+++ src/layouts/header/Navigation.tsx
@@ -0,0 +1,29 @@
+import { Group, ThemeIcon } from '@mantine/core';
+import { NavLink } from 'react-router-dom';
+import { HEADER_LINKS } from '../../constants/layout';
+
+function Navigation() {
+  const cls = (props: { isActive: boolean }) =>
+    `my-1 block text-gray-500 no-underline text-base py-2 px-4 mx-2 border-l-4 border-0 border-solid transition rounded ${
+      props.isActive
+        ? 'border-l-aurora bg-gray-100'
+        : 'border-l-transparent hover:border-l-aurora hover:bg-gray-100'
+    }`;
+
+  return (
+    <>
+      {HEADER_LINKS.map((link) => (
+        <NavLink to={link.path} key={link.path} className={cls}>
+          <Group>
+            <ThemeIcon color={link.color} variant='light'>
+              <link.icon />
+            </ThemeIcon>
+            <p className='m-0'>{link.label}</p>
+          </Group>
+        </NavLink>
+      ))}
+    </>
+  );
+}
+
+export default Navigation;
