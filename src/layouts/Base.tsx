7c541ad35b698ec7b3378439c1435b3979de0874
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-02 11:33:51 +0300

    drop back-to-top button

--- src/layouts/Base.tsx
+++ src/layouts/Base.tsx
@@ -3,7 +3,6 @@ import { Suspense, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Skeleton from '../common/Skeleton';
 import Aside from './Aside';
-import BackToTop from './BackToTop';
 import Header from './Header';
 import Navbar from './Navbar';
 
@@ -29,7 +28,6 @@ function Base(props: { minimal?: boolean }) {
     >
       <Suspense fallback={<Skeleton.post repeat={10} />}>
         <Outlet />
-        <BackToTop />
       </Suspense>
     </AppShell>
   );
