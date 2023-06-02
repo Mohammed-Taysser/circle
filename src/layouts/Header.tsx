556289ec1d20cefda7017a14ec79f8c724c3988f
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-06-02 19:01:10 +0300

    create header layout

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,34 +1,25 @@
-import { Group, Navbar, ScrollArea, ThemeIcon } from '@mantine/core';
-import { NavLink } from 'react-router-dom';
-import { HEADER_LINKS } from '../constants/layout';
+import { Navbar, ScrollArea } from '@mantine/core';
+import EventsTimeline from './header/EventsTimeline';
+import MiniCalender from './header/MiniCalender';
+import HeaderLink from './header/Navigation';
 
 function Header(props: { opened: boolean }) {
-  const cls = (props: { isActive: boolean }) =>
-    `my-1 block text-gray-500 no-underline text-base py-2 px-4 mx-2 border-r-4 border-0 border-solid transition ${
-      props.isActive
-        ? 'border-r-aurora'
-        : 'border-r-transparent hover:border-r-aurora'
-    }`;
-
   return (
     <Navbar
       p='md'
       hiddenBreakpoint='sm'
       hidden={!props.opened}
-      width={{ sm: 200, lg: 300 }}
-      zIndex={101}
+      width={{ sm: 300, lg: 400 }}
+      className='border-0 shadow-nice z-10'
     >
-      <Navbar.Section grow component={ScrollArea} mx='-xs'>
-        {HEADER_LINKS.map((link) => (
-          <NavLink to={link.path} key={link.path} className={cls}>
-            <Group>
-              <ThemeIcon color={link.color} variant='light'>
-                <link.icon />
-              </ThemeIcon>
-              <p className='m-0'>{link.label}</p>
-            </Group>
-          </NavLink>
-        ))}
+      <Navbar.Section mx='-xs' className='mb-10'>
+        <HeaderLink />
+      </Navbar.Section>
+      <Navbar.Section className='mb-6'>
+        <MiniCalender />
+      </Navbar.Section>
+      <Navbar.Section grow component={ScrollArea} type='auto'>
+        <EventsTimeline />
       </Navbar.Section>
     </Navbar>
   );
