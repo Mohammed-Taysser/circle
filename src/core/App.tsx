b30a33668b7a3b1bf7ee1f8e12edb176dd3f7f59
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-13 21:07:45 +0300

    add suspense & temporary add events modal

--- src/core/App.tsx
+++ src/core/App.tsx
@@ -7,13 +7,14 @@ import {
 } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
-import { useState } from 'react';
+import { Suspense, useState } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import CommentsModal from '../modal/Comments.modal';
+import EventsModal from '../modal/Events.modal';
 import ReactsModal from '../modal/Reacts.modal';
 import SearchModal from '../modal/Search.modal';
 import ErrorBoundary from './ErrorBoundary';
-import router from './Routes';
+import routes from './Routes';
 
 import '../assets/scss/app.scss';
 
@@ -43,16 +44,25 @@ function App() {
                 reacts: ReactsModal,
                 comments: CommentsModal,
                 search: SearchModal,
+                events: EventsModal,
               }}
             >
-              <RouterProvider
-                router={router}
-                fallbackElement={
+              <Suspense
+                fallback={
                   <Center h={200}>
                     <Loader color='teal' size='xl' variant='dots' />
                   </Center>
                 }
-              />
+              >
+                <RouterProvider
+                  router={routes}
+                  fallbackElement={
+                    <Center h={200}>
+                      <Loader color='teal' size='xl' variant='dots' />
+                    </Center>
+                  }
+                />
+              </Suspense>
             </ModalsProvider>
           </>
         </MantineProvider>
