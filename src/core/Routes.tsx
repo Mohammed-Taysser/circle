e68b6f2190ad4290f7b8c35bed7c1e50553666cc
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-13 21:07:13 +0300

    update login url to join-us

--- src/core/Routes.tsx
+++ src/core/Routes.tsx
@@ -11,7 +11,7 @@ function RequireAuth({ children }: { children: JSX.Element }) {
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
     // than dropping them off on the home page.
-    return <Navigate to='/login' state={{ next: location }} replace />;
+    return <Navigate to='/join-us' state={{ next: location }} replace />;
   }
 
   return children;
