7bbe3d91a80df09cbbda07e59f48ce04854fe395
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-13 20:48:06 +0300

    temporary create navbar layout

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,30 +1,96 @@
-import { Box, Navbar, ScrollArea } from '@mantine/core';
-import { MainLinks } from './_mainLinks';
-import { User } from './_user';
+import {
+  Burger,
+  Header,
+  Input,
+  Kbd,
+  MediaQuery,
+  ScrollArea,
+  useMantineTheme,
+} from '@mantine/core';
+import { modals } from '@mantine/modals';
+import { BiSearchAlt } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
+import Logo from './navbar/Logo';
+import UserDropdown from './navbar/UserDropdown';
 
-function Nav(props: { opened: boolean }) {
+function Navbar(props: NavbarProps) {
+  const theme = useMantineTheme();
+  const navigateTo = useNavigate();
+
+  const onSearchInputClick = () =>
+    modals.openContextModal({
+      modal: 'search',
+      title: '',
+      innerProps: {
+        navigateTo,
+      },
+      size: 'lg',
+      centered: true,
+      withCloseButton: false,
+      scrollAreaComponent: ScrollArea.Autosize,
+      overlayProps: {
+        color:
+          theme.colorScheme === 'dark'
+            ? theme.colors.dark[9]
+            : theme.colors.gray[2],
+        opacity: 0.55,
+        blur: 3,
+      },
+    });
+
   return (
-    <Navbar
-      p='md'
-      hiddenBreakpoint='sm'
-      hidden={!props.opened}
-      width={{ sm: 200, lg: 300 }}
-    >
-      <Navbar.Section grow component={ScrollArea} mx='-xs' px='xs'>
-        <Box py='md'>
-          <MainLinks />
-          <MainLinks />
-          <MainLinks />
-          <MainLinks />
-          <MainLinks />
-          <MainLinks />
-          <MainLinks />
-        </Box>
-      </Navbar.Section>
-      <Navbar.Section>
-        <User />
-      </Navbar.Section>
-    </Navbar>
+    <Header height={{ base: 50, md: 70 }} className='px-10'>
+      <div className='h-full grid grid-cols-5 items-center'>
+        <div className='col-span-1'>
+          <Logo />
+        </div>
+
+        <div className='col-span-1'>
+          <Input
+            className='flex-grow'
+            icon={<BiSearchAlt />}
+            radius='md'
+            onClick={onSearchInputClick}
+            component='button'
+            styles={(theme) => ({
+              input: {
+                cursor: 'pointer',
+                '&:focus-within': {
+                  borderColor: theme.colors.gray[4],
+                },
+              },
+            })}
+          >
+            <div className='text-gray-400 flex justify-between items-center relative top-[-1px]'>
+              <>Search</>
+              <Kbd className='text-gray-300 text-xs'>/</Kbd>
+            </div>
+          </Input>
+        </div>
+
+        <div className='col-span-2'>
+          <div className='flex justify-around items-center'>
+            <span>dropdowns</span>
+            <span>dropdowns</span>
+            <span>dropdowns</span>
+          </div>
+        </div>
+        <div className='col-span-1'>
+          <UserDropdown />
+        </div>
+
+        <MediaQuery largerThan='sm' styles={{ display: 'none' }}>
+          <Burger
+            opened={props.opened}
+            onClick={() => props.setOpened((o) => !o)}
+            size='sm'
+            color={theme.colors.gray[6]}
+            mr='xl'
+          />
+        </MediaQuery>
+      </div>
+    </Header>
   );
 }
-export default Nav;
+
+export default Navbar;
