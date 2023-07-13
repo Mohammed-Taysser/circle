5e4370720ac835ea87173d631e40e26c8602350a
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-13 21:08:23 +0300

    add navbar user-status

---
+++ src/constants/navbar.tsx
@@ -0,0 +1,16 @@
+const DROPDOWN_STATUS = [
+  {
+    color: '#32e4cd',
+    title: 'Online',
+  },
+  {
+    color: '#ffd52f',
+    title: 'Away',
+  },
+  {
+    color: '#cbdfee',
+    title: 'Invisible',
+  },
+];
+
+export { DROPDOWN_STATUS };
