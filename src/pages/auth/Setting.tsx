0d3a40a030e0361b36371526a87ae6d3568f47ca
Mohammed Taysser <mohamedtaysser112@yahoo.com>
2023-07-13 20:46:42 +0300

    temporary emprove setting page

--- src/pages/auth/Setting.tsx
+++ src/pages/auth/Setting.tsx
@@ -1,140 +1,40 @@
-import { useDocumentTitle, useUncontrolled } from '@mantine/hooks';
-import React from 'react';
-
-import {
-  Checkbox,
-  Image,
-  SimpleGrid,
-  Text,
-  UnstyledButton,
-  createStyles,
-  rem,
-} from '@mantine/core';
-
-const useStyles = createStyles((theme, { checked }: { checked: boolean }) => ({
-  button: {
-    display: 'flex',
-    alignItems: 'center',
-    width: '100%',
-    transition: 'background-color 150ms ease, border-color 150ms ease',
-    border: `${rem(1)} solid ${
-      checked
-        ? theme.fn.variant({ variant: 'outline', color: theme.primaryColor })
-            .border
-        : theme.colorScheme === 'dark'
-        ? theme.colors.dark[8]
-        : theme.colors.gray[3]
-    }`,
-    borderRadius: theme.radius.sm,
-    padding: theme.spacing.sm,
-    backgroundColor: checked
-      ? theme.fn.variant({ variant: 'light', color: theme.primaryColor })
-          .background
-      : theme.colorScheme === 'dark'
-      ? theme.colors.dark[8]
-      : theme.white,
-  },
-
-  body: {
-    flex: 1,
-    marginLeft: theme.spacing.md,
-  },
-}));
-
-interface ImageCheckboxProps {
-  checked?: boolean;
-  defaultChecked?: boolean;
-  onChange?(checked: boolean): void;
-  title: string;
-  description: string;
-  image: string;
-}
-
-export function ImageCheckbox({
-  checked,
-  defaultChecked,
-  onChange,
-  title,
-  description,
-  className,
-  image,
-  ...others
-}: ImageCheckboxProps &
-  Omit<React.ComponentPropsWithoutRef<'button'>, keyof ImageCheckboxProps>) {
-  const [value, handleChange] = useUncontrolled({
-    value: checked,
-    defaultValue: defaultChecked,
-    finalValue: false,
-    onChange,
-  });
-
-  const { classes, cx } = useStyles({ checked: value });
-
-  return (
-    <UnstyledButton
-      {...others}
-      onClick={() => handleChange(!value)}
-      className={cx(classes.button, className)}
-    >
-      <Image src={image} alt={title} width={40} />
-
-      <div className={classes.body}>
-        <Text color='dimmed' size='xs' sx={{ lineHeight: 1 }} mb={5}>
-          {description}
-        </Text>
-        <Text weight={500} size='sm' sx={{ lineHeight: 1 }}>
-          {title}
-        </Text>
-      </div>
-
-      <Checkbox
-        checked={value}
-        onChange={() => {}}
-        tabIndex={-1}
-        styles={{ input: { cursor: 'pointer' } }}
-      />
-    </UnstyledButton>
-  );
-}
-
-const mockdata = [
-  {
-    description: 'Sun and sea',
-    title: 'Beach vacation',
-    image: 'https://ui.mantine.dev/_next/static/media/sea.36b2d7d7.png',
-  },
-  {
-    description: 'Sightseeing',
-    title: 'City trips',
-    image: 'https://ui.mantine.dev/_next/static/media/city.93e713a1.png',
-  },
-  {
-    description: 'Mountains',
-    title: 'Hiking vacation',
-    image: 'https://ui.mantine.dev/_next/static/media/mountain.2e6150d2.png',
-  },
-  {
-    description: 'Snow and ice',
-    title: 'Winter vacation',
-    image: 'https://ui.mantine.dev/_next/static/media/winter.5c69fc85.png',
-  },
-];
+import { Tabs } from '@mantine/core';
+import { useDocumentTitle } from '@mantine/hooks';
+import { VscSettings } from 'react-icons/vsc';
+import Banner from '../../common/Banner';
+import { TOC } from '../../constants/setting';
 
 function Setting() {
   useDocumentTitle('Circle | Setting');
-  const items = mockdata.map((item) => (
-    <ImageCheckbox {...item} key={item.title} />
-  ));
+
   return (
-    <SimpleGrid
-      cols={4}
-      breakpoints={[
-        { maxWidth: 'md', cols: 2 },
-        { maxWidth: 'sm', cols: 1 },
-      ]}
-    >
-      {items}
-    </SimpleGrid>
+    <>
+      <Banner
+        title='Setting'
+        subtitle='Customize your preference'
+        icon={VscSettings}
+      />
+
+      <div className='shadow-nice p-10 rounded-lg mb-20 bg-white'>
+        <Tabs defaultValue={TOC[0].id} orientation='vertical'>
+          <Tabs.List>
+            {TOC.map((item) => (
+              <Tabs.Tab value={item.id} key={item.id}>
+                {item.label}
+              </Tabs.Tab>
+            ))}
+          </Tabs.List>
+
+          <div className='px-10 w-full'>
+            {TOC.map((item) => (
+              <Tabs.Panel value={item.id} key={item.id}>
+                <item.component />
+              </Tabs.Panel>
+            ))}
+          </div>
+        </Tabs>
+      </div>
+    </>
   );
 }
 
