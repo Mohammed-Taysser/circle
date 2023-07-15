93be0eab9676e875061434b16dd24641619ecd2b
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-15 15:16:46 +0300

    add toggle button to aside layout

--- src/layouts/Aside.tsx
+++ src/layouts/Aside.tsx
@@ -1,13 +1,39 @@
-import { Aside, MediaQuery, Text } from '@mantine/core';
-import React from 'react';
+import { Aside, Button, Text } from '@mantine/core';
+import { useState } from 'react';
+import { RiAppsLine } from 'react-icons/ri';
 
 function Side() {
+  const [isOpened, setIsOpened] = useState(false);
+
   return (
-    <MediaQuery smallerThan='lg' styles={{ display: 'none' }} className='z-10'>
-      <Aside p='md' hiddenBreakpoint='lg' width={{ md: 200, lg: 300 }}>
+    <>
+      <Aside
+        p='md'
+        hiddenBreakpoint='lg'
+        width={{ base: 300, lg: 350 }}
+        className={`border-0 shadow-nice z-40 duration-500 ${
+          isOpened ? '' : '-right-[300px] md:right-0'
+        }`}
+      >
+        <div className='relative md:hidden'>
+          <div
+            className={`absolute duration-500 ${
+              isOpened ? '-left-[70px]' : 'left-[-105px]'
+            }`}
+          >
+            <Button
+              variant='filled'
+              color=''
+              onClick={() => setIsOpened((prev) => !prev)}
+            >
+              <RiAppsLine className='text-lg' />
+            </Button>
+          </div>
+        </div>
+
         <Text>Application sidebar</Text>
       </Aside>
-    </MediaQuery>
+    </>
   );
 }
 
