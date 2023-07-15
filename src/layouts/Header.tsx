0246ad04f9a73d6db8b637a16f4ab8cc27fa6d84
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-15 15:15:31 +0300

    add props types & add toggle animation

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -3,21 +3,24 @@ import EventsTimeline from './header/EventsTimeline';
 import MiniCalender from './header/MiniCalender';
 import HeaderLink from './header/Navigation';
 
-function Header(props: { opened: boolean }) {
+function Header(props: HeaderProps) {
   return (
     <Navbar
       p='md'
-      hiddenBreakpoint='sm'
-      hidden={!props.opened}
-      width={{ sm: 300, lg: 400 }}
-      className='border-0 shadow-nice z-10'
+      hiddenBreakpoint='md'
+      width={{ base: 400 }}
+      className={`border-0 shadow-nice z-100 duration-500 ${
+        props.opened ? '' : 'left-[-100%] md:left-0'
+      }`}
     >
       <Navbar.Section mx='-xs' className='mb-10'>
         <HeaderLink />
       </Navbar.Section>
+
       <Navbar.Section className='mb-6'>
         <MiniCalender />
       </Navbar.Section>
+
       <Navbar.Section grow component={ScrollArea} type='auto'>
         <EventsTimeline />
       </Navbar.Section>
