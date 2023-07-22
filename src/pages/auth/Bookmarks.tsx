f8d344c407780f5289c75657d8c0a6426d021a28
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-22 12:41:03 +0300

    enhance bookmarks page

--- src/pages/auth/Bookmarks.tsx
+++ src/pages/auth/Bookmarks.tsx
@@ -1,9 +1,9 @@
-import { Pagination, Select } from '@mantine/core';
+import { Center, Pagination, Select } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useDocumentTitle } from '@mantine/hooks';
 import { useEffect, useState } from 'react';
-import { BsSortUpAlt } from 'react-icons/bs';
-import { VscFeedback } from 'react-icons/vsc';
+import { BsBookmarks, BsSortUpAlt } from 'react-icons/bs';
+import { TbTimelineEventText } from 'react-icons/tb';
 import Banner from '../../common/Banner';
 import Post from '../../common/Post';
 import Skeleton from '../../common/Skeleton';
@@ -12,6 +12,8 @@ import Async from '../../containers/Async';
 
 function Bookmarks() {
   useDocumentTitle('Circle | Bookmarks');
+  const posts = Math.random() < 0.5 ? [] : POSTS.filter((post) => post.isSaved);
+
   const [activePage, setPage] = useState(1);
 
   const searchForm = useForm({
@@ -47,43 +49,56 @@ function Bookmarks() {
     <>
       <Banner
         title='Bookmarks'
-        subtitle={`Bookmarks: ${POSTS.length}`}
-        icon={VscFeedback}
+        subtitle={`Bookmarks: ${posts.length}`}
+        icon={BsBookmarks}
       />
-      <div className='nice-shadow flex justify-between mb-10 p-5 bg-white'>
-        <form
-          className='flex items-center flex-1'
-          onSubmit={searchForm.onSubmit(onFormSearchFormSubmit)}
-        >
-          <Select
-            className='ml-auto inline-block'
-            icon={<BsSortUpAlt />}
-            placeholder='Filter By'
-            clearable
-            {...searchForm.getInputProps('sort')}
-            onChange={(value) => {
-              searchForm.setValues({
-                filter: value ?? 'updates',
-              });
-            }}
-            data={[
-              { value: 'updates', label: 'Updates' },
-              { value: 'photos', label: 'Photos' },
-              { value: 'videos', label: 'Videos' },
-              { value: 'audios', label: 'Audios' },
-            ]}
-          />
-        </form>
-      </div>
-      <Async {...state} skeleton={<Skeleton.post repeat={6} />}>
-        <div className={`my-10 `}>
-          {POSTS.map((post) => (
-            <Post post={post} key={post.id} />
-          ))}
-        </div>
-        <div className='nice-shadow p-5 flex justify-center my-10 bg-white'>
-          <Pagination value={activePage} onChange={setPage} total={10} />
-        </div>
+      <Async {...state} skeleton={<Skeleton variant='post' repeat={6} />}>
+        {posts.length ? (
+          <>
+            <div className='nice-shadow flex justify-between mb-10 p-5 bg-white'>
+              <form
+                className='flex items-center flex-1'
+                onSubmit={searchForm.onSubmit(onFormSearchFormSubmit)}
+              >
+                <Select
+                  className='ml-auto inline-block'
+                  icon={<BsSortUpAlt />}
+                  placeholder='Filter By'
+                  clearable
+                  {...searchForm.getInputProps('sort')}
+                  onChange={(value) => {
+                    searchForm.setValues({
+                      filter: value ?? 'updates',
+                    });
+                  }}
+                  data={[
+                    { value: 'updates', label: 'Updates' },
+                    { value: 'photos', label: 'Photos' },
+                    { value: 'videos', label: 'Videos' },
+                    { value: 'audios', label: 'Audios' },
+                  ]}
+                />
+              </form>
+            </div>
+            <div className={`my-10 `}>
+              {posts.map((post) => (
+                <Post post={post} key={post.id} />
+              ))}
+            </div>
+            <div className='nice-shadow p-5 flex justify-center my-10 bg-white'>
+              <Pagination value={activePage} onChange={setPage} total={10} />
+            </div>
+          </>
+        ) : (
+          <div className='shadow-nice p-4 bg-white rounded'>
+            <Center h={200}>
+              <div className='text-center text-gray-400'>
+                <TbTimelineEventText className='text-4xl' />
+                <p className='m-0'>Not Bookmarks yet</p>
+              </div>
+            </Center>
+          </div>
+        )}
       </Async>
     </>
   );
