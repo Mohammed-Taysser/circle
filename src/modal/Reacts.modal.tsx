1646e2882f31b5052379e480617503c9464fc8cc
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-22 12:10:16 +0300

    refactor reacts modal to be standalone with postId

--- src/modal/Reacts.modal.tsx
+++ src/modal/Reacts.modal.tsx
@@ -1,49 +1,72 @@
 import { Avatar, Center, Table, Tabs } from '@mantine/core';
+import { useEffect, useState } from 'react';
 import { VscReactions } from 'react-icons/vsc';
+import { POST_REACTS } from '../constants/dummy';
 import { REACT_ICONS } from '../constants/reacts';
+import Async from '../containers/Async';
 import { uuidv4 } from '../helpers';
 
 function ReactsModal({ innerProps }: ReactModalProps) {
-  const { reacts } = innerProps;
+  console.log(innerProps);
+
+  const reacts = Math.random() < 0.5 ? POST_REACTS : [];
+
+  const [state, setState] = useState({
+    loading: true,
+    fulfilled: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    const TimerId = setTimeout(() => {
+      setState({
+        loading: false,
+        fulfilled: true,
+        error: null,
+      });
+    }, 2000);
+
+    return () => {
+      clearTimeout(TimerId);
+    };
+  }, []);
 
   return (
-    <Tabs color='teal' defaultValue='all'>
-      <Tabs.List>
-        <Tabs.Tab value='all'>All</Tabs.Tab>
-        {Object.keys(reacts).map((key) => (
-          <Tabs.Tab value={key} key={key}>
-            {getReactIcon(key as PostReactsLabel)}
-          </Tabs.Tab>
-        ))}
-      </Tabs.List>
+    <Async {...state}>
+      <Tabs color='teal' defaultValue='all'>
+        <Tabs.List>
+          <Tabs.Tab value='all'>All</Tabs.Tab>
+          {Object.keys(reacts).map((key) => (
+            <Tabs.Tab value={key} key={key}>
+              <ReactIcon label={key as PostReactsLabel} />
+            </Tabs.Tab>
+          ))}
+        </Tabs.List>
 
-      <Tabs.Panel value='all' pt='xs'>
-        <Table>
-          <tbody>
-            <AllReactsRows reacts={reacts} />
-          </tbody>
-        </Table>
-      </Tabs.Panel>
-
-      {Object.entries(reacts).map(([key, value]) => (
-        <Tabs.Panel value={key} key={key} pt='xs'>
+        <Tabs.Panel value='all' pt='xs'>
           <Table>
             <tbody>
-              <ReactRows type={key} reacts={value as SinglePostReact[]} />
+              <AllReactsRows reacts={reacts} />
             </tbody>
           </Table>
         </Tabs.Panel>
-      ))}
-    </Tabs>
+
+        {Object.entries(reacts).map(([key, value]) => (
+          <Tabs.Panel value={key} key={key} pt='xs'>
+            <Table>
+              <tbody>
+                <ReactRows type={key} reacts={value} />
+              </tbody>
+            </Table>
+          </Tabs.Panel>
+        ))}
+      </Tabs>
+    </Async>
   );
 }
 
-const getReactIcon = (type: PostReactsLabel) => {
-  let info = REACT_ICONS[type];
-
-  if (!info) {
-    return <>No Icon</>;
-  }
+const ReactIcon = (props: { label: PostReactsLabel }) => {
+  let info = REACT_ICONS[props.label];
 
   return (
     <Avatar color={info.color} radius='sm'>
@@ -66,7 +89,9 @@ const ReactRows = (props: { reacts: SinglePostReact[]; type: string }) => {
           <td>
             <div className='text-gray-500'>@{react.username}</div>
           </td>
-          <td>{getReactIcon(props.type as PostReactsLabel)}</td>
+          <td>
+            <ReactIcon label={props.type as PostReactsLabel} />
+          </td>
         </tr>
       ))}
     </>
@@ -76,17 +101,22 @@ const ReactRows = (props: { reacts: SinglePostReact[]; type: string }) => {
 const AllReactsRows = (props: { reacts: PostReacts }) => {
   if (!Object.entries(props.reacts).length) {
     return (
-      <Center>
-        <div className='text-center my-4'>
-          <VscReactions className='text-3xl text-gray-400' />
-          <p className='text-gray-400 my-2'>No Reacts Yet</p>
-        </div>
-      </Center>
+      <tr>
+        <td>
+          <Center>
+            <div className='text-center my-4'>
+              <VscReactions className='text-3xl text-gray-400' />
+              <p className='text-gray-400 my-2'>No Reacts Yet</p>
+            </div>
+          </Center>
+        </td>
+      </tr>
     );
   }
+
   return (
     <>
-      {Object.entries(props.reacts).map(([type, react], index) => (
+      {Object.entries(props.reacts).map(([type, react]) => (
         <ReactRows key={type + uuidv4()} type={type} reacts={react} />
       ))}
     </>
