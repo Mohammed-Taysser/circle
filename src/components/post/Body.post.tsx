f38f3321198f29325af8f3b925ae780fd2ed2a00
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-22 20:49:46 +0300

    improve post body component

--- src/components/post/Body.post.tsx
+++ src/components/post/Body.post.tsx
@@ -1,36 +1,19 @@
-import { Anchor } from '@mantine/core';
-import parse from 'html-react-parser';
-import { Link } from 'react-router-dom';
-import { POST_TYPES } from '../../helpers';
+import Viewers from './Viewers';
+import TextContent from './body/TextContent';
 
 function BodyPost(props: { post: Post; full?: boolean }) {
-  const Body = POST_TYPES[props.post.variant].component;
-
   return (
     <div className='post-body'>
       <div className='activity-inner'>
-        <p className='widget-box-status-text'>
-          {props.full ? (
-            parse(props.post.body)
-          ) : (
-            <>
-              {parse(props.post.body.substring(0, 1000))}
-              {props.post.body.length >= 1000 && (
-                <>
-                  <span className='mx-1'>.....</span>
-                  <Anchor
-                    component={Link}
-                    to={`/post/${props.post.id}`}
-                    className='mb-3 inline-block'
-                  >
-                    Continue Reading
-                  </Anchor>
-                </>
-              )}
-            </>
-          )}
-        </p>
-        <Body post={props.post} full={props.full} />
+        <TextContent
+          id={props.post.id}
+          body={props.post.body}
+          full={props.full}
+        />
+
+        {props.post.variant !== 'update' && (
+          <Viewers post={props.post} full={props.full} />
+        )}
       </div>
     </div>
   );
