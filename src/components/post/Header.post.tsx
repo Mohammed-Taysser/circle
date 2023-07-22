96cce53cdbb25a1a2b056f9940e1f71b30e26776
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-22 20:51:18 +0300

    improve post header component

--- src/components/post/Header.post.tsx
+++ src/components/post/Header.post.tsx
@@ -1,62 +1,67 @@
-import { Anchor, Indicator } from '@mantine/core';
+import { Anchor, Flex, Indicator } from '@mantine/core';
 import { FiExternalLink } from 'react-icons/fi';
+import { VscVerified } from 'react-icons/vsc';
 import { Link } from 'react-router-dom';
-import RoundedAvatar from '../../common/Avatar';
-import { POST_TYPES, timeToX } from '../../helpers';
+import Avatar from '../../common/Avatar';
+import { timeToX } from '../../helpers';
 import PostDropdown from './header/PostDropdown';
+import Visibility from './header/Visibility';
 
-function HeaderPost(props: { post: Post; full?: boolean }) {
-  if (!props.post) {
-    return <></>;
-  }
+function HeaderPost(props: PostHeaderProps) {
+  const { post } = props;
 
   return (
     <>
       <div className='post-header'>
         <div className='media'>
           <div className='activity-avatar'>
-            <Link to={`/profile/${props.post.user.id}`}>
+            <Link to={`/profile/${post.user.id}`}>
               <Indicator
                 size={12}
                 offset={7}
                 position='bottom-end'
-                color=''
+                color='' // TODO: replace with socket.io
                 withBorder
                 zIndex={10}
               >
-                <RoundedAvatar sm alt='avatar' src={props.post.user.avatar} />
+                <Avatar
+                  sm
+                  alt={post.user.name + ' avatar'}
+                  src={post.user.avatar}
+                />
               </Indicator>
             </Link>
             <div className='status-info ml-4'>
               <div className='activity-title'>
-                <Link to={`/profile/${props.post.user.id}`}>
-                  {props.post.user.name}
-                </Link>
+                <Link to={`/profile/${post.user.id}`}>{post.user.name}</Link>
+                {post.user.isVerified && (
+                  <VscVerified className='text-lg text-aurora' />
+                )}
                 <span className='block md:inline-block text-sm'>
-                  {POST_TYPES[props.post.variant].msg}
+                  {post.activity}
                 </span>
               </div>
-              <div className='activity-time'>
-                <span>{timeToX(props.post.publishAt)}</span>
+              <Flex gap={10} align='center'>
+                <span className='activity-time'>{timeToX(post.publishAt)}</span>
+                {post.editAt && <span className='activity-time'>Edited</span>}
+                <Visibility visibility={post.visibility} />
                 {!props.full && (
-                  <Anchor
-                    component={Link}
-                    to={`/post/${props.post.id}`}
-                    className='mx-1'
-                  >
+                  <Anchor component={Link} to={`/post/${post.id}`}>
                     <FiExternalLink />
                   </Anchor>
                 )}
-              </div>
+              </Flex>
             </div>
           </div>
         </div>
         <div>
-          <PostDropdown
-            id={props.post.id}
-            body={props.post.body}
-            isSaved={props.post.isSaved}
-          />
+          {!props.isShared && (
+            <PostDropdown
+              id={post.id}
+              body={post.body}
+              isSaved={post.isSaved}
+            />
+          )}
         </div>
       </div>
     </>
