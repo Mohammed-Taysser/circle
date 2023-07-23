c7c15d41560b3a36752efb6ca980cd935a832e7b
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-23 20:48:06 +0300

    create notification page

---
+++ src/pages/auth/Notification.tsx
@@ -0,0 +1,53 @@
+import { Flex, Pagination } from '@mantine/core';
+import { useDocumentTitle } from '@mantine/hooks';
+import { useEffect, useState } from 'react';
+import SingleNotification from '../../components/notification/SingleNotification';
+import { NOTIFICATIONS } from '../../constants/layout';
+import Async from '../../containers/Async';
+
+function Notification() {
+  useDocumentTitle('Circle | Notifications');
+
+  const [state, setState] = useState({
+    loading: true,
+    fulfilled: false,
+    error: null,
+  });
+
+  useEffect(() => {
+    const TimerId = setTimeout(() => {
+      setState({
+        loading: false,
+        fulfilled: true,
+        error: null,
+      });
+    }, 2000);
+
+    return () => {
+      clearTimeout(TimerId);
+    };
+  }, []);
+
+  return (
+    <Async {...state}>
+      <>
+        <div className='mb-10 nice-shadow p-7 bg-white rounded relative'>
+          <h2 className='first-letter:text-4xl first-letter:text-aurora text-xl font-bold mt-0'>
+            Activity
+          </h2>
+
+          <Flex direction='column' wrap='wrap' gap={10}>
+            {NOTIFICATIONS.map((request) => (
+              <SingleNotification request={request} key={request.id} />
+            ))}
+          </Flex>
+        </div>
+        <div className='mb-10 nice-shadow p-7 bg-white rounded relative'>
+          <Pagination total={10} withEdges position='center' />
+        </div>
+      </>
+    </Async>
+  );
+}
+
+export default Notification;
