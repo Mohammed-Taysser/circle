a5391a331f8e060d898b5da576f85e30e2190ebc
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-23 09:02:46 +0300

    add loading placeholder to odf viewer

--- src/components/post/body/viewers/Pdf.tsx
+++ src/components/post/body/viewers/Pdf.tsx
@@ -1,9 +1,26 @@
-import { AspectRatio } from '@mantine/core';
+import { AspectRatio, LoadingOverlay } from '@mantine/core';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 function PDF(props: { src: string }) {
+  const [isLoading, setIsLoading] = useState(true);
+  const pdfRef = useRef<HTMLEmbedElement>(null);
+
+  useEffect(() => {
+    pdfRef.current?.addEventListener('load', onPdfLoad);
+
+    return () => {
+      pdfRef.current?.removeEventListener('load', onPdfLoad);
+    };
+  }, []);
+
+  const onPdfLoad = useCallback(() => {
+    setIsLoading(false);
+  }, []);
+
   return (
-    <AspectRatio ratio={16 / 9}>
-      <embed src={props.src} />
+    <AspectRatio ratio={16 / 9} pos='relative'>
+      <LoadingOverlay visible={isLoading} overlayBlur={2} />
+      <embed src={props.src} ref={pdfRef} />
     </AspectRatio>
   );
 }
