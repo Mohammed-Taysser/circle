fec3662ae0795828518c9946a6389953ceca9f69
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-23 20:52:41 +0300

    add `SingleNotification` to layout types

--- src/types/layout.d.ts
+++ src/types/layout.d.ts
@@ -7,3 +7,19 @@ interface HeaderProps {
   opened: boolean;
   setOpened: React.Dispatch<React.SetStateAction<boolean>>;
 }
+
+interface SingleNotification {
+  type: string;
+  id: string;
+  unread: boolean;
+  date: Date;
+  post: {
+    id: string;
+  };
+  user: {
+    name: string;
+    avatar: string;
+    id: string;
+  };
+  msg: string;
+}
