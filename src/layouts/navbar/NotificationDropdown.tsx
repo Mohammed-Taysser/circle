8dc4742b128f2e0a57406d219ad7333acb243f63
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-23 20:51:59 +0300

    navbar: improve notification dropdown

--- src/layouts/navbar/NotificationDropdown.tsx
+++ src/layouts/navbar/NotificationDropdown.tsx
@@ -1,5 +1,4 @@
 import {
-  Avatar,
   Button,
   Flex,
   Indicator,
@@ -10,140 +9,10 @@ import {
   UnstyledButton,
 } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
-import { LiaShareSolid } from 'react-icons/lia';
 import { MdOutlineNotificationsActive } from 'react-icons/md';
-import { TbMessage2Bolt } from 'react-icons/tb';
-import { TfiShare } from 'react-icons/tfi';
 import { Link } from 'react-router-dom';
-import avatar from '../../assets/images/default/avatar.png';
-import { timeToX } from '../../helpers';
-
-const NOTIFICATION = [
-  {
-    type: 'comment',
-    id: 1,
-    unread: true,
-    date: new Date(),
-    user: {
-      name: 'Mohammed Taysser',
-      avatar,
-      id: 1,
-    },
-    msg: `commented on your profile avatar`,
-  },
-  {
-    type: 'replay',
-    id: 2,
-    unread: true,
-    date: new Date(),
-    user: {
-      name: 'Tony Stevens',
-      avatar: 'https://html.crumina.net/html-olympus/img/avatar56-sm.webp',
-      id: 1,
-    },
-    msg: `replay on your comment`,
-  },
-  {
-    type: 'share',
-    id: 3,
-    unread: false,
-    date: new Date(),
-    user: {
-      name: 'Tamara Romanoff',
-      avatar: 'https://html.crumina.net/html-olympus/img/avatar55-sm.webp',
-      id: 1,
-    },
-    msg: `share a new status`,
-  },
-  {
-    type: 'replay',
-    id: 4,
-    unread: true,
-    date: new Date(),
-    user: {
-      name: 'Mary Jane Stark',
-      avatar: 'https://html.crumina.net/html-olympus/img/avatar57-sm.webp',
-      id: 1,
-    },
-    msg: `replay on your comment`,
-  },
-  {
-    type: 'comment',
-    id: 5,
-    unread: false,
-    date: new Date(),
-    user: {
-      name: 'Stagg Clothing',
-      avatar: 'https://html.crumina.net/html-olympus/img/avatar58-sm.webp',
-      id: 1,
-    },
-    msg: `commented on your profile cover`,
-  },
-  {
-    type: 'share',
-    id: 6,
-    unread: false,
-    date: new Date(),
-    user: {
-      name: 'Jake Parker',
-      avatar: 'https://html.crumina.net/html-olympus/img/avatar60-sm.webp',
-      id: 1,
-    },
-    msg: `share a new image`,
-  },
-  {
-    type: 'replay',
-    id: 7,
-    unread: true,
-    date: new Date(),
-    user: {
-      name: 'Elaine Dreyfuss',
-      avatar: 'https://html.crumina.net/html-olympus/img/avatar61-sm.webp',
-      id: 1,
-    },
-    msg: `replay on your comment`,
-  },
-  {
-    type: 'share',
-    id: 8,
-    unread: false,
-    date: new Date(),
-    user: {
-      name: 'Aalaa Kamal',
-      avatar: 'https://html.crumina.net/html-olympus/img/avatar65-sm.webp',
-      id: 1,
-    },
-    msg: `share a new status`,
-  },
-];
-
-const RequestIcon = (props: { type: string }) => {
-  switch (props.type) {
-    case 'share':
-      return <TfiShare />;
-    case 'replay':
-      return <LiaShareSolid />;
-    case 'comment':
-      return <TbMessage2Bolt />;
-
-    default:
-      return <MdOutlineNotificationsActive />;
-  }
-};
-
-const getIconColor = (type: string) => {
-  switch (type) {
-    case 'share':
-      return 'pink';
-    case 'replay':
-      return 'violet';
-    case 'comment':
-      return 'orange';
-
-    default:
-      return 'teal';
-  }
-};
+import SingleNotification from '../../components/notification/SingleNotification';
+import { NOTIFICATIONS } from '../../constants/layout';
 
 function NotificationDropdown() {
   const isSmallerThanMd = useMediaQuery('(min-width: 56.25em)');
@@ -181,68 +50,9 @@ function NotificationDropdown() {
         </Text>
 
         <ScrollArea.Autosize mah={300} type='auto' offsetScrollbars>
-          <Flex direction='column' wrap='wrap' gap={20}>
-            {NOTIFICATION.map((request) => (
-              <Link
-                to={`/message/${request.user.id}`}
-                key={request.id}
-                className='no-underline'
-              >
-                <Flex
-                  align='center'
-                  justify='space-between'
-                  gap={10}
-                  className='hover:bg-gray-50 p-4 duration-200 rounded'
-                >
-                  <Avatar
-                    src={request.user.avatar}
-                    radius='xl'
-                    alt={request.user.name}
-                  />
-
-                  <Flex
-                    align='center'
-                    justify='space-between'
-                    gap={10}
-                    className={`flex-1 ${request.unread ? '' : 'opacity-50'}`}
-                  >
-                    <div className={`flex-1 `}>
-                      <Text
-                        size='sm'
-                        weight={500}
-                        color='dark'
-                        display='inline-block'
-                      >
-                        {request.user.name}
-                      </Text>{' '}
-                      <Text
-                        color='dimmed'
-                        size='xs'
-                        display='inline-block'
-                        weight={request.unread ? 800 : 500}
-                      >
-                        {request.msg}
-                      </Text>
-                      <Text
-                        color='dimmed'
-                        size='xs'
-                        mt={5}
-                        weight={request.unread ? 800 : 500}
-                      >
-                        {timeToX(request.date)}
-                      </Text>
-                    </div>
-                    <ThemeIcon
-                      variant='light'
-                      size='lg'
-                      color={getIconColor(request.type)}
-                      radius='lg'
-                    >
-                      <RequestIcon type={request.type} />
-                    </ThemeIcon>
-                  </Flex>
-                </Flex>
-              </Link>
+          <Flex direction='column' wrap='wrap' gap={10}>
+            {NOTIFICATIONS.map((request) => (
+              <SingleNotification request={request} key={request.id} />
             ))}
           </Flex>
         </ScrollArea.Autosize>
