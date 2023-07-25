8a1dd846d2ae132e0e0d9eea90416bc98163888b
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-25 21:06:56 +0300

    create calender help start page

---
+++ src/pages/public/help/Calender.help.tsx
@@ -0,0 +1,23 @@
+import { useDocumentTitle } from '@mantine/hooks';
+import React from 'react';
+
+function CalenderHelp() {
+  useDocumentTitle('Circle | Help | Calender');
+
+  return (
+    <div className='events-page shadow-nice p-4 rounded mb-20 bg-white'>
+      <div className='mb-10'>
+        <h2>Instructions</h2>
+        <ul>
+          <li>Select dates and you will be prompted to create a new event</li>
+          <li>Drag, drop, and resize events</li>
+          <li>Click an event to delete it</li>
+          <li>Click on day number to show day events</li>
+          <li>Click on week number to show week events</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default CalenderHelp;
