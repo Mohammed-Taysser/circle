fe0e7add3d5987b1d216635eb2df137083798fc1
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-25 21:05:36 +0300

    create help page

---
+++ src/pages/public/Help.tsx
@@ -0,0 +1,58 @@
+import { Anchor, Image } from '@mantine/core';
+import { useDocumentTitle } from '@mantine/hooks';
+import { TbProgressHelp } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+import calenderImage from '../../assets/images/background/help/calender.svg';
+import editorImage from '../../assets/images/background/help/editor.svg';
+import Banner from '../../common/Banner';
+import { uuidv4 } from '../../helpers';
+
+const HELP_PAGES = [
+  {
+    img: calenderImage,
+    title: 'Calender',
+    url: 'calender',
+  },
+  {
+    img: editorImage,
+    title: 'Editor',
+    url: 'editor',
+  },
+];
+
+function Help() {
+  useDocumentTitle('Circle | Help');
+
+  return (
+    <>
+      <Banner
+        title='Help'
+        subtitle={`Find what you need`}
+        icon={TbProgressHelp}
+      />
+      <div className='md:grid grid-cols-2 lg:grid-cols-3 gap-4'>
+        {HELP_PAGES.map((groupType) => (
+          <div className='col' key={uuidv4()}>
+            <div className='nice-shadow p-5 bg-white'>
+              <Image
+                withPlaceholder
+                height={200}
+                src={groupType.img}
+                alt={`${groupType.title}-help`}
+              />
+              <Anchor
+                component={Link}
+                to={`/help/${groupType.url}`}
+                className='mt-3 inline-block text-2xl no-underline'
+              >
+                {groupType.title}
+              </Anchor>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Help;
