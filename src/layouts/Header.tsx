666b199622c82282c40b3ef7bad06fdea4d64872
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-28 11:28:35 +0300

    header layout: fix zIndex issue

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -9,7 +9,7 @@ function Header(props: HeaderProps) {
       p='md'
       hiddenBreakpoint='md'
       width={{ base: 400 }}
-      className={`border-0 shadow-nice z-100 duration-500 ${
+      className={`border-0 shadow-nice z-[1000] duration-500 ${
         props.opened ? '' : 'left-[-100%] md:left-0'
       }`}
     >
