227a57a3fdc04341595fc3ba973492791c134842
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-29 17:15:12 +0300

    add messages to layout constant file

--- src/constants/layout.tsx
+++ src/constants/layout.tsx
@@ -6,6 +6,7 @@ import { MdEventNote } from 'react-icons/md';
 import { VscFeedback } from 'react-icons/vsc';
 import avatar from '../assets/images/default/avatar.png';
 
+// Header
 const HEADER_LINKS = [
   {
     icon: VscFeedback,
@@ -33,6 +34,7 @@ const HEADER_LINKS = [
   },
 ];
 
+// Navbar
 const USER_DROPDOWN_LINKS = [
   {
     icon: FiUser,
@@ -41,7 +43,7 @@ const USER_DROPDOWN_LINKS = [
   },
   {
     icon: BiMessageDetail,
-    path: '/messenger',
+    path: '/message',
     label: 'Messenger',
   },
   {
@@ -56,24 +58,123 @@ const USER_DROPDOWN_LINKS = [
   },
 ];
 
-const NAVBAR_DROPDOWN_STATUS = [
+const MESSAGES = [
   {
+    unread: true,
+    status: 'online',
+    id: 1,
+    date: new Date(),
+    user: {
+      name: 'Mohammed Taysser',
+      avatar,
+      id: 1,
+    },
+    msg: `Hi James! It's Diana, I just wanted to let you know that we have to reschedule`,
+  },
+  {
+    unread: true,
+    status: 'online',
+    id: 2,
+    date: new Date(),
+    user: {
+      name: 'Tony Stevens',
+      avatar: 'https://html.crumina.net/html-olympus/img/avatar56-sm.webp',
+      id: 2,
+    },
+    msg: `Great, I'll see you tomorrow!.`,
+  },
+  {
+    unread: false,
+    status: 'away',
+    id: 3,
+    date: new Date(),
+    user: {
+      name: 'Tamara Romanoff',
+      avatar: 'https://html.crumina.net/html-olympus/img/avatar55-sm.webp',
+      id: 3,
+    },
+    msg: `We'll have to check that at the office and see if the client is on board with`,
+  },
+  {
+    unread: true,
+    status: 'offline',
+    id: 4,
+    date: new Date(),
+    user: {
+      name: 'Mary Jane Stark',
+      avatar: 'https://html.crumina.net/html-olympus/img/avatar57-sm.webp',
+      id: 4,
+    },
+    msg: 'Yeah! Seems fine by me!',
+  },
+  {
+    unread: false,
+    id: 5,
+    date: new Date(),
+    status: 'away',
+    user: {
+      name: 'Stagg Clothing',
+      avatar: 'https://html.crumina.net/html-olympus/img/avatar58-sm.webp',
+      id: 5,
+    },
+    msg: `We'll have to check that at the office and see if the client is on board with`,
+  },
+  {
+    unread: false,
+    id: 6,
+    date: new Date(),
+    status: 'offline',
+    user: {
+      name: 'Jake Parker',
+      avatar: 'https://html.crumina.net/html-olympus/img/avatar60-sm.webp',
+      id: 6,
+    },
+    msg: `Hi James! It's Diana, I just wanted to let you know that we have to reschedule`,
+  },
+  {
+    unread: true,
+    id: 7,
+    date: new Date(),
+    status: 'online',
+    user: {
+      name: 'Elaine Dreyfuss',
+      avatar: 'https://html.crumina.net/html-olympus/img/avatar61-sm.webp',
+      id: 7,
+    },
+    msg: '4 Friends in Common',
+  },
+  {
+    unread: false,
+    id: 8,
+    date: new Date(),
+    status: 'invisible',
+    user: {
+      name: 'Aalaa Kamal',
+      avatar: 'https://html.crumina.net/html-olympus/img/avatar59-sm.webp',
+      id: 8,
+    },
+    msg: `We'll have to check that at the office and see if the client is on board with`,
+  },
+];
+
+const USER_STATUS: UserStatus = {
+  online: {
     color: '#20c997',
     title: 'Online',
   },
-  {
+  away: {
     color: '#fcc419',
     title: 'Away',
   },
-  {
+  invisible: {
     color: '#dee2e6',
     title: 'Invisible',
   },
-  {
+  offline: {
     color: '#f03e3e',
     title: 'Offline',
   },
-];
+};
 
 const NOTIFICATIONS: SingleNotification[] = [
   {
@@ -200,7 +301,8 @@ const NOTIFICATIONS: SingleNotification[] = [
 
 export {
   HEADER_LINKS,
-  NAVBAR_DROPDOWN_STATUS,
+  USER_STATUS,
   USER_DROPDOWN_LINKS,
   NOTIFICATIONS,
+  MESSAGES,
 };
