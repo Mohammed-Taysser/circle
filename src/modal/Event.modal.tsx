2a7687c15f1bdad11af7b535b52755b7a1aef3d7
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-29 17:14:15 +0300

    update event details modal

--- src/modal/Event.modal.tsx
+++ src/modal/Event.modal.tsx
@@ -1,7 +1,69 @@
+import { Button, Center, Flex, Text } from '@mantine/core';
 import { ContextModalProps } from '@mantine/modals';
+import { timeToX } from '../helpers';
+import { useState } from 'react';
+import { notifications } from '@mantine/notifications';
+import { BsInfoCircle } from 'react-icons/bs';
 
 function EventModal(props: ContextModalProps<EventModalInnerProps>) {
-  return <div>{JSON.stringify(props.innerProps.event)}</div>;
+  const { event } = props.innerProps;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDeleteEventClick = () => {
+    event.remove();
+
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+      notifications.show({
+        title: 'Successfully delete event',
+        message: 'Hey there, your event is successfully deleted!',
+        withCloseButton: true,
+        autoClose: true,
+      });
+
+      props.context.closeModal(props.id);
+    }, 2000);
+  };
+
+  return (
+    <div>
+      <Flex align='center' gap={10}>
+        <Text size='lg' weight={500} color='dark'>
+          {event.title}
+        </Text>
+        <Text size='sm' weight={500} color='dimmed'>
+          {timeToX(event.end)}
+        </Text>
+      </Flex>
+
+      {event.extendedProps.info ? (
+        <Text color='dimmed' size='xs' weight={800}>
+          {event.extendedProps.info}
+        </Text>
+      ) : (
+        <Center h={200}>
+          <div className='text-center text-gray-400'>
+            <BsInfoCircle className='text-4xl' />
+            <p className='m-0'>Not info for this event</p>
+          </div>
+        </Center>
+      )}
+
+      <Flex justify='end' mt={20}>
+        <Button
+          variant='outline'
+          color='red'
+          onClick={onDeleteEventClick}
+          loading={isLoading}
+        >
+          Delete
+        </Button>
+      </Flex>
+    </div>
+  );
 }
 
 export default EventModal;
