a2f8d030129c94b6c6b50c98c268fb17492533b7
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-31 11:16:08 +0300

    add friend-request & follow buttons

--- src/pages/public/Profile.tsx
+++ src/pages/public/Profile.tsx
@@ -1,7 +1,7 @@
-import { Tooltip } from '@mantine/core';
+import { Button, Flex, Text, Tooltip } from '@mantine/core';
 import { BiBookReader, BiUserVoice } from 'react-icons/bi';
 import { BsPersonBadge, BsPersonVideo3 } from 'react-icons/bs';
-import { FiUserPlus, FiUsers } from 'react-icons/fi';
+import { FiUserCheck, FiUserPlus, FiUserX, FiUsers } from 'react-icons/fi';
 import { IoImagesOutline } from 'react-icons/io5';
 import { MdOutlineAudiotrack } from 'react-icons/md';
 import { RiQuillPenLine } from 'react-icons/ri';
@@ -13,10 +13,97 @@ import cover from '../../assets/images/default/cover.jpg';
 import InfoBanner from '../../common/InfoBanner';
 import Taps from '../../common/Taps';
 import useHelmet from '../../hooks/useHelmet';
+import { useState } from 'react';
+import { notifications } from '@mantine/notifications';
+import { modals } from '@mantine/modals';
 
 function Profile() {
-  useHelmet('profile');
+  useHelmet('profile'); // TODO: improve SEO
   const { profileId = '' } = useParams();
+  const isFriend = Math.random() < 0.5; // TODO: find suitable algorithm
+  const isFollow = Math.random() < 0.5; // TODO: find suitable algorithm
+  const [isLoading, setIsLoading] = useState({
+    friend: false,
+    follow: false,
+  });
+
+  const onAddFriendBtnClick = () => {
+    setIsLoading((prev) => ({ ...prev, friend: true }));
+
+    setTimeout(() => {
+      setIsLoading((prev) => ({ ...prev, friend: false }));
+
+      notifications.show({
+        title: 'Successfully Friend request send',
+        message: 'Hey there, an request had been send to mohammed!',
+        loading: false,
+        withCloseButton: true,
+        autoClose: true,
+      });
+    }, 2000);
+  };
+
+  const onFollowBtnClick = () => {
+    setIsLoading((prev) => ({ ...prev, follow: true }));
+
+    setTimeout(() => {
+      setIsLoading((prev) => ({ ...prev, follow: false }));
+
+      notifications.show({
+        title: 'Successfully follow',
+        message: 'Hey there, Successfully follow mohammed!',
+        loading: false,
+        withCloseButton: true,
+        autoClose: true,
+      });
+    }, 2000);
+  };
+
+  const onUnFollowBtnClick = () => {
+    setIsLoading((prev) => ({ ...prev, follow: true }));
+
+    setTimeout(() => {
+      setIsLoading((prev) => ({ ...prev, follow: false }));
+
+      notifications.show({
+        title: 'Successfully un follow',
+        message: 'Hey there, Successfully un follow mohammed!',
+        loading: false,
+        withCloseButton: true,
+        autoClose: true,
+      });
+    }, 2000);
+  };
+
+  const onUnfriendBtnClick = () => {
+    modals.openConfirmModal({
+      title: 'Please confirm your action',
+      children: (
+        <Text size='sm'>
+          This action is so important that you are required to confirm it. Are
+          you sure to unfriend <strong>Mohammed Taysser</strong>
+        </Text>
+      ),
+      labels: { confirm: 'Yes, Leave', cancel: 'Cancel' },
+      onConfirm: onUnfriendConfirmBtnClick,
+    });
+  };
+
+  const onUnfriendConfirmBtnClick = () => {
+    setIsLoading((prev) => ({ ...prev, friend: true }));
+
+    setTimeout(() => {
+      setIsLoading((prev) => ({ ...prev, friend: false }));
+
+      notifications.show({
+        title: 'Successfully unfriend',
+        message: 'Hey there, you successfully unfriend mohammed',
+        loading: false,
+        withCloseButton: true,
+        autoClose: true,
+      });
+    }, 2000);
+  };
 
   const TAPS = [
     {
@@ -65,34 +152,84 @@ function Profile() {
         name='Mohammed Taysser'
         verified
         extraInfo={
-          <div className='mb-2 flex items-center justify-center md:justify-start gap-4'>
-            <Tooltip label='Friends' withArrow>
-              <div className='flex items-center gap-1'>
-                <FiUsers /> <span className='text-sm'>12</span>
-              </div>
-            </Tooltip>
-            <Tooltip label='Posts' withArrow>
-              <div className='flex items-center gap-1'>
-                <VscFeedback /> <span className='text-sm'>12</span>
-              </div>
-            </Tooltip>
-            <Tooltip label='Followers' withArrow>
-              <div className='flex items-center gap-1'>
-                <BiUserVoice />
-                <span className='text-sm'>303,202,2</span>
-              </div>
-            </Tooltip>
-            <Tooltip label='Following' withArrow>
-              <div className='flex items-center gap-1'>
-                <FiUserPlus /> <span className='text-sm'>1023</span>
-              </div>
-            </Tooltip>
-            <Tooltip label='Viewers' withArrow>
-              <div className='flex items-center gap-1'>
-                <BiBookReader /> <span className='text-sm'>258941</span>
-              </div>
-            </Tooltip>
-          </div>
+          <Flex
+            justify='space-between'
+            direction={{ sm: 'row', base: 'column' }}
+          >
+            <div className='flex items-center justify-center md:justify-start gap-4'>
+              <Tooltip label='Friends' withArrow>
+                <div className='flex items-center gap-1'>
+                  <FiUsers /> <span className='text-sm'>12</span>
+                </div>
+              </Tooltip>
+              <Tooltip label='Posts' withArrow>
+                <div className='flex items-center gap-1'>
+                  <VscFeedback /> <span className='text-sm'>12</span>
+                </div>
+              </Tooltip>
+              <Tooltip label='Followers' withArrow>
+                <div className='flex items-center gap-1'>
+                  <BiUserVoice />
+                  <span className='text-sm'>303,202,2</span>
+                </div>
+              </Tooltip>
+              <Tooltip label='Following' withArrow>
+                <div className='flex items-center gap-1'>
+                  <FiUserPlus /> <span className='text-sm'>1023</span>
+                </div>
+              </Tooltip>
+              <Tooltip label='Viewers' withArrow>
+                <div className='flex items-center gap-1'>
+                  <BiBookReader /> <span className='text-sm'>258941</span>
+                </div>
+              </Tooltip>
+            </div>
+            <Flex
+              gap={10}
+              className='mt-4 md:mt-0 justify-center md:justify-start'
+            >
+              {isFriend ? (
+                <Button
+                  size='xs'
+                  loading={isLoading.friend}
+                  onClick={onAddFriendBtnClick}
+                  leftIcon={<FiUserPlus className='text-lg' />}
+                >
+                  Add Friend
+                </Button>
+              ) : (
+                <Button
+                  variant='default'
+                  size='xs'
+                  loading={isLoading.friend}
+                  onClick={onUnfriendBtnClick}
+                  leftIcon={<FiUserX className='text-lg' />}
+                >
+                  Unfriend
+                </Button>
+              )}
+              {isFollow ? (
+                <Button
+                  size='xs'
+                  loading={isLoading.follow}
+                  onClick={onFollowBtnClick}
+                  leftIcon={<FiUserCheck className='text-lg' />}
+                >
+                  Follow
+                </Button>
+              ) : (
+                <Button
+                  size='xs'
+                  variant='default'
+                  loading={isLoading.follow}
+                  onClick={onUnFollowBtnClick}
+                  leftIcon={<FiUserX className='text-lg' />}
+                >
+                  Un follow
+                </Button>
+              )}
+            </Flex>
+          </Flex>
         }
       />
       <Taps taps={TAPS} />
