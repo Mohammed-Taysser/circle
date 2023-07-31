cf20adde1490d999e7fe1f3e905862b1409c5907
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-07-31 11:16:22 +0300

    add join button

--- src/pages/public/SingleGroup.tsx
+++ src/pages/public/SingleGroup.tsx
@@ -1,6 +1,11 @@
-import { Tooltip } from '@mantine/core';
+import { Button, Flex, Text, Tooltip } from '@mantine/core';
 import { useDocumentTitle } from '@mantine/hooks';
+import { modals } from '@mantine/modals';
+import { notifications } from '@mantine/notifications';
+import { useState } from 'react';
+import { AiOutlineUsergroupAdd } from 'react-icons/ai';
 import { BsPersonBadge, BsPersonVideo3 } from 'react-icons/bs';
+import { FaPersonRunning } from 'react-icons/fa6';
 import { FiUsers } from 'react-icons/fi';
 import { IoImagesOutline } from 'react-icons/io5';
 import { MdOutlineAudiotrack } from 'react-icons/md';
@@ -14,8 +19,10 @@ import InfoBanner from '../../common/InfoBanner';
 import Taps from '../../common/Taps';
 
 function SingleGroup() {
-  useDocumentTitle('Circle | Developer Geeks');
+  useDocumentTitle('Circle | Developer Geeks'); // TODO: improve SEO
   const { groupId = '' } = useParams();
+  const isJoined = Math.random() < 0.5; // TODO: find suitable algorithm
+  const [isLoading, setIsLoading] = useState(false);
 
   const TAPS = [
     {
@@ -55,6 +62,52 @@ function SingleGroup() {
     },
   ];
 
+  const onJoinBtnClick = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+      notifications.show({
+        title: 'Successfully join',
+        message: 'Hey there, welcome to Developer Geeks!',
+        loading: false,
+        withCloseButton: true,
+        autoClose: true,
+      });
+    }, 2000);
+  };
+
+  const onLeaveBtnClick = () => {
+    modals.openConfirmModal({
+      title: 'Please confirm your action',
+      children: (
+        <Text size='sm'>
+          This action is so important that you are required to confirm it. Are
+          you sure to leave <strong>Developer Geeks</strong>
+        </Text>
+      ),
+      labels: { confirm: 'Yes, Leave', cancel: 'Cancel' },
+      onConfirm: onLeaveConfirmBtnClick,
+    });
+  };
+
+  const onLeaveConfirmBtnClick = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+      notifications.show({
+        title: 'Successfully leave',
+        message: 'Hey there, you successfully leave Developer Geeks group',
+        loading: false,
+        withCloseButton: true,
+        autoClose: true,
+      });
+    }, 2000);
+  };
+
   return (
     <div>
       <InfoBanner
@@ -64,23 +117,47 @@ function SingleGroup() {
         name='Developer Geeks'
         verified
         extraInfo={
-          <div className='mb-2 flex items-center justify-center md:justify-start '>
-            <Tooltip label='Members' withArrow>
-              <div className='flex items-center'>
-                <FiUsers /> <span className='mx-1 text-sm'>1200</span>
-              </div>
-            </Tooltip>
-            <Tooltip label='Posts' withArrow>
-              <div className='flex items-center mx-1'>
-                <VscFeedback /> <span className='mx-1 text-sm'>102</span>
-              </div>
-            </Tooltip>
-            <Tooltip label='Visibility' withArrow>
-              <div className='flex items-center mx-1'>
-                <TbEyeglass /> <span className='mx-1 text-sm'>Public</span>
-              </div>
-            </Tooltip>
-          </div>
+          <Flex justify='space-between'>
+            <div className='flex-1 flex gap-3 items-center justify-center md:justify-start '>
+              <Tooltip label='Members' withArrow>
+                <div className='flex items-center gap-1 '>
+                  <FiUsers /> <span className='text-sm'>1200</span>
+                </div>
+              </Tooltip>
+              <Tooltip label='Posts' withArrow>
+                <div className='flex items-center gap-1'>
+                  <VscFeedback /> <span className='text-sm'>102</span>
+                </div>
+              </Tooltip>
+              <Tooltip label='Visibility' withArrow>
+                <div className='flex items-center gap-1'>
+                  <TbEyeglass /> <span className='text-sm'>Public</span>
+                </div>
+              </Tooltip>
+            </div>
+            <div>
+              {isJoined ? (
+                <Button
+                  size='xs'
+                  loading={isLoading}
+                  onClick={onJoinBtnClick}
+                  leftIcon={<AiOutlineUsergroupAdd className='text-lg' />}
+                >
+                  Join
+                </Button>
+              ) : (
+                <Button
+                  variant='default'
+                  size='xs'
+                  loading={isLoading}
+                  onClick={onLeaveBtnClick}
+                  leftIcon={<FaPersonRunning className='text-lg' />}
+                >
+                  Leave
+                </Button>
+              )}
+            </div>
+          </Flex>
         }
       />
       <Taps taps={TAPS} />
